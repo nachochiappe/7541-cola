e8a607f2a39654396863dce2af94a2726149faf7
Ignacio Chiappe <nacho.chiappe@gmail.com>
2015-09-12 11:48:24 -0300

    Versión inicial de pruebas.

---
+++ pruebas_alumno.c
@@ -0,0 +1,13 @@
+#include "cola.h"
+#include "testing.h"
+#include <stddef.h>
+#include <stdio.h>
+
+
+/* ******************************************************************
+ *                   PRUEBAS UNITARIAS ALUMNO
+ * *****************************************************************/
+
+void prueba_pila_alumno() {
+    
+}
